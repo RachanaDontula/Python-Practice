1657f5b11985c6cf9292cff26c5e748a2b410340
RachanaDontula <50663501+RachanaDontula@users.noreply.github.com>
2019-09-12 16:35:46 +0800

    Add files via upload

---
+++ prob-2.py
@@ -0,0 +1,6 @@
+'''
+Write a Python program to get the Python version you are using
+'''
+# import system package and then print system version directly.
+import sys
+print("The Python version is: ",sys.version)
