237af36c0266c843f522b9700db4cf1992ac4a6f
RachanaDontula <50663501+RachanaDontula@users.noreply.github.com>
2019-09-12 17:56:59 +0800

    Add files via upload

---
+++ prob-4.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program which accepts the radius of a circle from the user and compute the area. Go to the editor
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504
+"""
+
+pi = 3.147
+r = float(input("Enter teh radius value: "))
+print("Area = ", pi*r*r)
